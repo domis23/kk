fb423a7d041cf3f5423a49c18ff11790db3094d4
domis23 <96925584+domis23@users.noreply.github.com>
2024-10-18 17:07:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ document.querySelector(".go").addEventListener('click', () => {
     var surname = document.querySelector(".surname");
     var image = document.querySelector(".image");
 
-    var params = new URLSearchParams();
-    params.set("firstname", firstname.value);
-    params.set("surname", surname.value);
-    params.set("image", image.value);
-    location.href = "/yObywatel/id?" + params;
+   
 });
